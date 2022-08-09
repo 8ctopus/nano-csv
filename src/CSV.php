e65716a1d4484583fc7ecc2b413a3cdaac706e95
8ctopus <hello@octopuslabs.io>
2022-08-09 09:22:35 +0500

    refactored: csv class

--- src/CSV.php
+++ src/CSV.php
@@ -2,18 +2,8 @@
 
 namespace Oct8pus\CSV;
 
-class CSV
+class CSV extends File
 {
-    private $handle;
-
-    private string $file;
-    private int $size;
-    private int $startOffset;
-    private int $currentOffset;
-
-    private BOM $bom;
-    private LineEnding $lineEnding;
-
     private string $separator;
     private string $enclosure;
     private string $escape;
@@ -31,13 +21,7 @@ class CSV
      */
     public function __construct(string $file)
     {
-        if (!file_exists($file)) {
-            throw new CSVException('file does not exist');
-        }
-
-        $this->file = $file;
-
-        $this->currentOffset = 0;
+        parent::__construct($file);
     }
 
     /**
@@ -51,11 +35,7 @@ class CSV
         $header = $this->header ? 'true' : 'false';
 
         return
-            "file: {$this->file}" . PHP_EOL .
-            "size: {$this->size}" . PHP_EOL .
-            "BOM: {$this->bom->debug()}" . PHP_EOL .
-            "encoding: {$this->encoding}" . PHP_EOL .
-            "line ending: {$this->lineEnding->toStr()}" . PHP_EOL .
+            parent::__toString() .
             "separator: {$this->separator}" . PHP_EOL .
             "enclosure: {$this->enclosure}" . PHP_EOL .
             "header: {$header}" . PHP_EOL .
@@ -71,50 +51,7 @@ class CSV
      */
     public function autoDetect() : self
     {
-        // open file
-        $this->handle = fopen($this->file, 'r', false, null);
-
-        if ($this->handle === false) {
-            throw new CSVException('open file');
-        }
-
-        // get file info
-        $stat = fstat($this->handle);
-
-        if ($stat === false) {
-            throw new CSVException('file stat');
-        }
-
-        // get size
-        $this->size = $stat['size'];
-
-        if ($this->size === 0) {
-            throw new CSVException('empty file');
-        }
-
-        // get bom
-        $this->bom = $this->getBOM();
-
-        // set data start offset
-        $this->startOffset = $this->bom->startOffset();
-        $this->currentOffset = $this->startOffset;
-
-        // seek to where data starts
-        if (fseek($this->handle, $this->currentOffset, SEEK_SET) !== 0) {
-            throw new CSVException('fseek');
-        }
-
-        // set encoding
-        $this->encoding = $this->bom->encoding();
-
-        // read part of file
-        $text = $this->read($this->size > 500 ? 500 : $this->size, true);
-
-        if (empty($this->encoding)) {
-            $this->encoding = $this->detectEncoding($text);
-        }
-
-        $this->lineEnding = $this->detectLineEnding($text);
+        parent::autoDetect();
 
         $this->separator = $this->detectSeparator();
 
@@ -148,27 +85,12 @@ class CSV
      */
     public function readRow(int $row) : array
     {
-        // save offset
-        $offset = $this->currentOffset;
-
-        if (fseek($this->handle, $this->startOffset, SEEK_SET) !== 0) {
-            throw new CSVException('fseek');
-        }
-
         if (isset($this->header) && $this->header) {
             $row += 1;
         }
 
         for ($i = 0; $i <= $row; ++$i) {
-            $line = $this->readLine(false);
-        }
-
-        if (isset($offset)) {
-            if (fseek($this->handle, $offset, SEEK_SET) !== 0) {
-                throw new CSVException('fseek');
-            }
-
-            $this->currentOffset = $offset;
+            $line = parent::readCurrentLine(false);
         }
 
         return $this->lineToArray($line);
@@ -181,7 +103,7 @@ class CSV
      */
     public function readNextRow() : ?array
     {
-        $line = $this->readLine(false);
+        $line = parent::readCurrentLine(false);
 
         if (!$line) {
             return null;
@@ -191,190 +113,13 @@ class CSV
     }
 
     /**
-     * Read line
-     *
-     * @param bool $resetOffset
-     *
-     * @return ?string
-     */
-    private function readLine(bool $resetOffset) : ?string
-    {
-        if ($this->currentOffset >= $this->size) {
-            return null;
-        }
-
-        $offset = $this->currentOffset;
-
-        $str = '';
-        $length = 100;
-        $read = 0;
-        $end = false;
-
-        while (1) {
-            // check for end of file
-            if ($read + $length + $offset > $this->size) {
-                $length = $this->size - $offset - $read;
-                $end = true;
-            }
-
-            $str .= $this->read($length, false);
-            $read += $length;
-
-            $position = mb_strpos($str, $this->lineEnding->ending(), 0);
-
-            if ($position !== false || $end) {
-                $line = mb_substr($str, 0, $end ? null : $position);
-
-                if ($resetOffset) {
-                    $this->currentOffset = $offset;
-                } else {
-                    $this->currentOffset = $offset + strlen(mb_convert_encoding($line, $this->encoding, 'UTF-8')) + strlen(mb_convert_encoding($this->lineEnding->ending(), $this->encoding, 'UTF-8'));
-                }
-
-                if (fseek($this->handle, $this->currentOffset, SEEK_SET) !== 0) {
-                    throw new CSVException('fseek');
-                }
-
-                return $line;
-            }
-        }
-
-        throw new CSVException();
-    }
-
-    /**
-     * Read from file
-     *
-     * @param int  $length
-     * @param bool $resetOffset
-     *
-     * @return string
-     */
-    private function read(int $length, bool $resetOffset) : string
-    {
-        if ($length <= 0) {
-            throw new CSVException('invalid length');
-        }
-
-        // save offset
-        if ($resetOffset) {
-            $offset = $this->currentOffset;
-        }
-
-        if ($this->currentOffset + $length > $this->size) {
-            $pos = $this->currentOffset + $length;
-            throw new CSVException("out of bounds {$pos} / {$this->size}");
-        }
-
-        $str = fread($this->handle, $length);
-
-        if ($str === false) {
-            throw new CSVException('fread');
-        }
-
-        if (isset($offset)) {
-            if (fseek($this->handle, $offset, SEEK_SET) !== 0) {
-                throw new CSVException('fseek');
-            }
-
-            $this->currentOffset = $offset;
-        } else {
-            $this->currentOffset += $length;
-        }
-
-        if (!empty($this->encoding)) {
-            $str = mb_convert_encoding($str, 'UTF-8', $this->encoding);
-        }
-
-        if ($str === false) {
-            throw new CSVException('convert encoding');
-        }
-
-        return $str;
-    }
-
-    /**
-     * Get byte order mark (bom)
-     *
-     * @return BOM
-     */
-    private function getBOM() : BOM
-    {
-        $data = str_split($this->read($this->size > 3 ? 3 : $this->size, true));
-
-        $boms = [
-            BOM::Utf8->encoding() => [0xEF, 0xBB, 0xBF],
-            BOM::Utf16LE->encoding() => [0xFF, 0xFE],
-            BOM::Utf16BE->encoding() => [0xFE, 0xFF],
-        ];
-
-        foreach ($boms as $name => $bom) {
-            for ($i = 0; $i < sizeof($bom); ++$i) {
-                if ($bom[$i] !== ord($data[$i])) {
-                    break;
-                }
-
-                return BOM::fromStr($name);
-            }
-        }
-
-        return BOM::None;
-    }
-
-    /**
-     * Detect encoding
-     *
-     * @param string $text
-     *
-     * @throws CSVException
-     *
-     * @return string
-     */
-    private function detectEncoding(string $text) : string
-    {
-        $encoding = mb_detect_encoding($text, ['auto'], true);
-
-        if (!$encoding) {
-            throw new CSVException('detect encoding');
-        }
-
-        return $encoding;
-    }
-
-    /**
-     * Detect line ending
-     *
-     * @param string $text
-     *
-     * @throws CSVException
-     *
-     * @return LineEnding
-     */
-    private function detectLineEnding(string $text) : LineEnding
-    {
-        $endings = [
-            LineEnding::Windows->toStr() => "\r\n",
-            LineEnding::Linux->toStr() => "\n",
-            LineEnding::Mac->toStr() => "\r",
-        ];
-
-        foreach ($endings as $name => $ending) {
-            if (str_contains($text, $ending)) {
-                return LineEnding::fromStr($name);
-            }
-        }
-
-        throw new CSVException('detect line ending');
-    }
-
-    /**
      * Detect field separator
      *
      * @return string
      */
     private function detectSeparator() : string
     {
-        $line = $this->readLine(true);
+        $line = parent::readCurrentLine(true);
 
         $separators = [
             ',' => 0,
@@ -396,7 +141,7 @@ class CSV
      */
     private function readColumns() : array
     {
-        $line = $this->readLine(true);
+        $line = parent::readCurrentLine(true);
 
         return $this->lineToArray($line);
     }
@@ -435,7 +180,7 @@ class CSV
     private function detectEnclosure() : string
     {
         /* alternate way
-        $line = $this->readLine(true);
+        $line = parent::readCurrentLine(true);
 
         $enclosures = [
             '"' => 0,
