ad8684a531c5c277f6c56cb8d232c09421caf1c2
8ctopus <hello@octopuslabs.io>
2023-02-14 14:19:54 +0400

    fixed: switch case block

--- src/XLSX.php
+++ src/XLSX.php
@@ -50,33 +50,34 @@ class XLSX extends CSV
 
         if ($result !== true) {
             switch ($result) {
-                case ZipArchive::ER_OK           : $result = 'No error';
-                case ZipArchive::ER_MULTIDISK    : $result = 'Multi-disk zip archives not supported';
-                case ZipArchive::ER_RENAME       : $result = 'Renaming temporary file failed';
-                case ZipArchive::ER_CLOSE        : $result = 'Closing zip archive failed';
-                case ZipArchive::ER_SEEK         : $result = 'Seek error';
-                case ZipArchive::ER_READ         : $result = 'Read error';
-                case ZipArchive::ER_WRITE        : $result = 'Write error';
-                case ZipArchive::ER_CRC          : $result = 'CRC error';
-                case ZipArchive::ER_ZIPCLOSED    : $result = 'Containing zip archive was closed';
-                case ZipArchive::ER_NOENT        : $result = 'No such file';
-                case ZipArchive::ER_EXISTS       : $result = 'File already exists';
-                case ZipArchive::ER_OPEN         : $result = 'Can\'t open file';
-                case ZipArchive::ER_TMPOPEN      : $result = 'Failure to create temporary file';
-                case ZipArchive::ER_ZLIB         : $result = 'Zlib error';
-                case ZipArchive::ER_MEMORY       : $result = 'Malloc failure';
-                case ZipArchive::ER_CHANGED      : $result = 'Entry has been changed';
-                case ZipArchive::ER_COMPNOTSUPP  : $result = 'Compression method not supported';
-                case ZipArchive::ER_EOF          : $result = 'Premature EOF';
-                case ZipArchive::ER_INVAL        : $result = 'Invalid argument';
-                case ZipArchive::ER_NOZIP        : $result = 'Not a zip archive';
-                case ZipArchive::ER_INTERNAL     : $result = 'Internal error';
-                case ZipArchive::ER_INCONS       : $result = 'Zip archive inconsistent';
-                case ZipArchive::ER_REMOVE       : $result = 'Can\'t remove file';
-                case ZipArchive::ER_DELETED      : $result = 'Entry has been deleted';
+                case ZipArchive::ER_OK           : $result = 'No error'; break;
+                case ZipArchive::ER_MULTIDISK    : $result = 'Multi-disk zip archives not supported'; break;
+                case ZipArchive::ER_RENAME       : $result = 'Renaming temporary file failed'; break;
+                case ZipArchive::ER_CLOSE        : $result = 'Closing zip archive failed'; break;
+                case ZipArchive::ER_SEEK         : $result = 'Seek error'; break;
+                case ZipArchive::ER_READ         : $result = 'Read error'; break;
+                case ZipArchive::ER_WRITE        : $result = 'Write error'; break;
+                case ZipArchive::ER_CRC          : $result = 'CRC error'; break;
+                case ZipArchive::ER_ZIPCLOSED    : $result = 'Containing zip archive was closed'; break;
+                case ZipArchive::ER_NOENT        : $result = 'No such file'; break;
+                case ZipArchive::ER_EXISTS       : $result = 'File already exists'; break;
+                case ZipArchive::ER_OPEN         : $result = 'Can\'t open file'; break;
+                case ZipArchive::ER_TMPOPEN      : $result = 'Failure to create temporary file'; break;
+                case ZipArchive::ER_ZLIB         : $result = 'Zlib error'; break;
+                case ZipArchive::ER_MEMORY       : $result = 'Malloc failure'; break;
+                case ZipArchive::ER_CHANGED      : $result = 'Entry has been changed'; break;
+                case ZipArchive::ER_COMPNOTSUPP  : $result = 'Compression method not supported'; break;
+                case ZipArchive::ER_EOF          : $result = 'Premature EOF'; break;
+                case ZipArchive::ER_INVAL        : $result = 'Invalid argument'; break;
+                case ZipArchive::ER_NOZIP        : $result = 'Not a zip archive'; break;
+                case ZipArchive::ER_INTERNAL     : $result = 'Internal error'; break;
+                case ZipArchive::ER_INCONS       : $result = 'Zip archive inconsistent'; break;
+                case ZipArchive::ER_REMOVE       : $result = 'Can\'t remove file'; break;
+                case ZipArchive::ER_DELETED      : $result = 'Entry has been deleted'; break;
 
                 default:
                     $result = sprintf('Unknown status %s', $result);
+                    break;
             }
 
             throw new CSVException("open xlsx - {$result}");
